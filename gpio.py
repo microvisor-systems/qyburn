945849fda9516b20244c504c9c01979107c11420
Surya Vasudev <suryavasudev05@gmail.com>
2025-11-01 22:06:23 -0400

    red led

--- gpio.py
+++ gpio.py
@@ -1,16 +1,19 @@
 from gpiozero import LED
 from time import sleep
 
+red_led = LED(4)
 green_led = LED(26)
 blue_led = LED(17)
 yellow_led = LED(21)
 
 while True:
+    red_led.on()
     green_led.on()
     blue_led.on()
     yellow_led.on()
     sleep(1)
-    green_led.on()
-    blue_led.on()
+    red_led.off()
+    green_led.off()
+    blue_led.off()
     yellow_led.off()
     sleep(1)
